befe9f944b01e3b57130bd34a4484391fd4129e1
Massimiliano Falsini <falsinimassimiliano@gmail.com>
2023-03-29 16:09:56 +0200

    ReLU

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate ndarray_csv;
 use csv::ReaderBuilder;
 use ndarray::Array;
 use ndarray::Array2;
+use ndarray::Zip;
 use ndarray_csv::Array2Reader;
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
@@ -16,7 +17,7 @@ fn get_csv_shape() -> Vec<usize> {
     vec![42000, 785]
 }
 
-fn init_params(csv_shape: Vec<usize>) -> Vec<Array2<f64>> {
+fn init_params(csv_shape: Vec<usize>) -> Vec<Array2<f32>> {
     let w1 = Array::random((10, csv_shape[1] - 1), Uniform::new(-0.5, 0.5));
     let b1 = Array::random((10, 1), Uniform::new(-0.5, 0.5));
     let w2 = Array::random((10, 10), Uniform::new(-0.5, 0.5));
@@ -24,30 +25,35 @@ fn init_params(csv_shape: Vec<usize>) -> Vec<Array2<f64>> {
     vec![w1, b1, w2, b2]
 }
 
-fn ReLU(z: Array2<f64>) -> Array2<f64> {
-    // TO DO
-    let pippo = Array2::<f64>::zeros((1, 1));
-    pippo
+fn re_l_u(z: Array2<f32>) -> Array2<f32> {
+    let mut x = Array2::<f32>::zeros((z.shape()[0], z.shape()[1]));
+    Zip::from(&mut x).and(&z).for_each(|a, &b| {
+        if b > 0.0 {
+            *a = b
+        }
+    });
+    x
 }
 
-fn softmax(z: Array2<f64>) {
+fn softmax(z: Array2<f32>) {
     // TO DO
 }
 
-fn forward_propagation(w1: Array2<f64>, b1: Array2<f64>, w2: Array2<f64>, b2: Array2<f64>) {
-    let x = Array2::<f64>::zeros((1, 1)); // TO DO
+fn forward_propagation(w1: Array2<f32>, b1: Array2<f32>, w2: Array2<f32>, b2: Array2<f32>) {
+    let x = Array2::<f32>::zeros((1, 1)); // TO DO
 
     let z1 = w1.dot(&x) + b1;
-    let a1 = ReLU(z1);
+    let a1 = re_l_u(z1);
     let z2 = w2.dot(&a1) + b2;
     let a2 = softmax(z2);
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let csv_shape = get_csv_shape();
+
     // Read an array back from the file
     let file = File::open("mnist_train.csv")?;
     let mut reader = ReaderBuilder::new().has_headers(true).from_reader(file);
-    let csv_shape = get_csv_shape();
     let array_read: Array2<u8> = reader.deserialize_array2((csv_shape[0], csv_shape[1]))?;
     println!("{}", array_read);
 
@@ -60,7 +66,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{}", params[2].clone());
 
     // Forward propagation
-    // forward_propagation(w1, b1, w2, b2);
+    forward_propagation(w1, b1, w2, b2);
 
     Ok(())
 }
