54e53bfbba6596a3786e1dd6787e784f1ef996ca
Massimiliano Falsini <falsinimassimiliano@gmail.com>
2023-03-30 16:07:34 +0200

    back prop

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ extern crate ndarray_csv;
 
 use csv::ReaderBuilder;
 use ndarray::Array;
+use ndarray::Array1;
 use ndarray::Array2;
+use ndarray::Axis;
 use ndarray::Zip;
 use ndarray_csv::Array2Reader;
 use ndarray_rand::rand_distr::Uniform;
@@ -52,6 +54,41 @@ fn forward_propagation(w1: Array2<f32>, b1: Array2<f32>, w2: Array2<f32>, b2: Ar
     let a2 = softmax(z2);
 }
 
+fn one_hot(y: Array2<f32>) -> Array2<f32> {
+    // TO DO
+    y
+}
+
+fn re_l_u_derivative(z: Array2<f32>) -> Array2<f32> {
+    let mut x = z.clone();
+    for k in x.iter_mut() {
+        if *k > 0.0 {
+            *k = 1.0
+        }
+    }
+    x
+}
+
+fn backward_prop(
+    z1: Array2<f32>,
+    a1: Array2<f32>,
+    // z2: Array2<f32>,
+    a2: Array2<f32>,
+    w2: Array2<f32>,
+    y: Array2<f32>,
+    x: Array2<f32>,
+) -> (Array2<f32>, f32, Array2<f32>, Array1<f32>) {
+    let m = y.len() as f32;
+    let one_hot_y = one_hot(y);
+    let dz2 = a2 - one_hot_y;
+    let dw2 = 1.0 / m * dz2.dot(&a1.t());
+    let db2 = 1.0 / m + dz2.sum_axis(Axis(2));
+    let dz1 = w2.t().dot(&dz2) * re_l_u_derivative(z1);
+    let dw1 = 1.0 / m * dz1.dot(&x.t());
+    let db1 = 1.0 / m + dz1.sum();
+    (dw1, db1, dw2, db2)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let csv_shape = get_csv_shape();
 
@@ -59,17 +96,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("mnist_train.csv")?;
     let mut reader = ReaderBuilder::new().has_headers(true).from_reader(file);
     let array_read: Array2<u8> = reader.deserialize_array2((csv_shape[0], csv_shape[1]))?;
-    println!("{}", array_read);
 
     // Init params
     let params = init_params(csv_shape);
-    let w1 = params[0].clone();
-    let b1 = params[1].clone();
-    let w2 = params[2].clone();
-    let b2 = params[3].clone();
-
-    // Forward propagation
-    forward_propagation(w1, b1, w2, b2);
 
     Ok(())
 }
